7fd0113948076be931bfa1df41e6a3f1e05e0c7e
ThiagoMiranda40 <contato@thiagomirandaoficial.com.br>
2022-09-03 03:12:52 -0300

    Criando método construtor

--- OOAula3/src/Carro.java
+++ OOAula3/src/Carro.java
@@ -7,42 +7,46 @@ public class Carro {
 
     // Construtor aqui
 
+    Carro () {
+        
+    }
+
 
     // Métodos Get e Set da cor (preciso aprofundar no conceito e para que serve esses dois tipos de método)
 
-        void SetCor (String cor) {
-            this.cor = cor;
-        }
+    void SetCor (String cor) {
+        this.cor = cor;
+    }
 
-        String GetCor () {
-            return cor;
-        }
+    String GetCor () {
+        return cor;
+    }
 
     // Métodos Get e Set do Modelo (Set serve para definir, ou seja, setar ou colocar um valor no atributo, e o get serve para pegar, ou seja, retornar algo)
 
-        void SetModelo (String modelo) {
-            this.modelo = modelo;
-        }
+    void SetModelo (String modelo) {
+        this.modelo = modelo;
+    }
 
-        String GetModelo () {
-            return modelo;
-        }
+    String GetModelo () {
+        return modelo;
+    }
 
     // Get e Set do Tanque
 
-        void SetCapacidadeTanque (int CapacidadeTanque) {
-            this.capacidadeTanque = capacidadeTanque;
-        }
+    void SetCapacidadeTanque (int CapacidadeTanque) {
+        this.capacidadeTanque = capacidadeTanque;
+    }
 
-        Int GetCapacidadeTanque () {
-            return capacidadeTanque;
-        }
+    Int GetCapacidadeTanque () {
+        return capacidadeTanque;
+    }
     
-    // Método para calcular total para ecnher o tanque (usar o tipo double que pode representar números fracionados e também mais casas decimais)
+    // Método calcular total para ecnher o tanque (usar o tipo double que pode representar números fracionados e também mais casas decimais)
 
-        double calcularEncherTanque (double valorCombustivel) {
-            return capacidadeTanque * valorCombustivel;
-        }
+    double calcularEncherTanque (double valorCombustivel) {
+        return capacidadeTanque * valorCombustivel;
+    }
         
 
 }
