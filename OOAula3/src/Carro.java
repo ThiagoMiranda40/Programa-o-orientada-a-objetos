a0e474e37ae9f00fba9a1250ed443f47370c533d
ThiagoMiranda40 <contato@thiagomirandaoficial.com.br>
2022-09-03 03:39:36 -0300

    Criação do Método Construtor + 1ª sobrecarga

--- OOAula3/src/Carro.java
+++ OOAula3/src/Carro.java
@@ -3,17 +3,16 @@
 public class Carro {
     String cor;
     String modelo;
-    int capacidadeTanque
+    int capacidadeTanque;
 
-    // Construtor aqui
-
-    Carro () {
-        
+    // Método Construtor + 1ª Sobrecaga do método construtor
+    Carro (String cor, String modelo, int capacidadeTanque) {
+        this.cor = cor;
+        this.modelo = modelo;
+        this.capacidadeTanque = capacidadeTanque;
     }
 
-
     // Métodos Get e Set da cor (preciso aprofundar no conceito e para que serve esses dois tipos de método)
-
     void SetCor (String cor) {
         this.cor = cor;
     }
@@ -23,7 +22,6 @@ public class Carro {
     }
 
     // Métodos Get e Set do Modelo (Set serve para definir, ou seja, setar ou colocar um valor no atributo, e o get serve para pegar, ou seja, retornar algo)
-
     void SetModelo (String modelo) {
         this.modelo = modelo;
     }
@@ -33,17 +31,15 @@ public class Carro {
     }
 
     // Get e Set do Tanque
-
-    void SetCapacidadeTanque (int CapacidadeTanque) {
+    void SetCapacidadeTanque (int capacidadeTanque) {
         this.capacidadeTanque = capacidadeTanque;
     }
 
-    Int GetCapacidadeTanque () {
+    int GetCapacidadeTanque () {
         return capacidadeTanque;
     }
     
     // Método calcular total para ecnher o tanque (usar o tipo double que pode representar números fracionados e também mais casas decimais)
-
     double calcularEncherTanque (double valorCombustivel) {
         return capacidadeTanque * valorCombustivel;
     }
