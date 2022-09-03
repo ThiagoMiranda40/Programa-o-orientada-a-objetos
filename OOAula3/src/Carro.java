1cae79f79459cffac6f114d909ce2dd515703700
ThiagoMiranda40 <contato@thiagomirandaoficial.com.br>
2022-09-03 02:39:14 -0300

    Criação dos métodos Get e Set

--- OOAula3/src/Carro.java
+++ OOAula3/src/Carro.java
@@ -3,6 +3,30 @@
 public class Carro {
     String cor;
     String modelo;
-    int capacidadeTanque;
+    
+    int capacidadeTanque
+
+    // Construtor aqui
+
+
+    // Métodos Get e Set da cor (preciso aprofundar no conceito e para que serve esses dois tipos de método)
+
+        void SetCor (String cor) {
+            this.cor = cor;
+        }
+
+        String GetCor () {
+            return cor;
+        }
+
+    // Métodos Get e Set do Modelo (Set serve para definir, ou seja, setar algo, e o get serve para pegar, ou seja, retornar algo)
+        void SetModelo (String modelo) {
+            this.modelo = modelo;
+        }
+
+        String GetModelo () {
+            return modelo;
+        }
+
     
 }
