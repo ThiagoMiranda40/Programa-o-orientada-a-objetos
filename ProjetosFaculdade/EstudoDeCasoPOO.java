50cfb6bc33174cd97eab568df83aa9ee015ec20a
ThiagoMiranda40 <contato@thiagomirandaoficial.com.br>
2022-09-27 22:28:22 -0300

    Criando Classes

---
+++ ProjetosFaculdade/EstudoDeCasoPOO.java
@@ -0,0 +1,14 @@
+public class Veículo {
+    String placa;
+    int ano;
+
+        public class Caminhao {
+
+        }
+
+        public class Onibus {
+            
+        }
+
+
+}
--- ProjetosFaculdade/ProjetoFaculdade1.java
+++
@@ -1,5 +0,0 @@
-package ProjetosFaculdade;
-
-public class ProjetoFaculdade1 {
-    
-}
